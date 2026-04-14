d6b59b6d47ad1e6af73bad7c874df33e5808e47c
RadnyM <rmiko03@wp.pl>
2026-04-14 17:07:49 +0200

    Added ability to upload, delete and favourite photos using Cloudinary API. Fixed a typo in MemerUpdateDto.cs ("Description" => "Description")

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { EditableMember, Member, Photo } from '../../types/member';
-import { tap } from 'rxjs';
+import { from, tap } from 'rxjs';
+import { form } from '@angular/forms/signals';
 
 @Injectable({
   providedIn: 'root',
@@ -31,5 +32,19 @@ export class MemberService {
 
   updateMember(member: EditableMember) {
     return this.http.put(this.baseUrl + 'members', member);
+  }
+
+  uploadPhoto(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(this.baseUrl + 'members/add-photo', formData);
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'members/delete-photo/' + photoId);
   }
 }
