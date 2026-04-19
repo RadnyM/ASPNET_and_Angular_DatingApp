86d18dcb1b4781c17b41fb401acf44d2f969f862
RadnyM <rmiko03@wp.pl>
2026-04-19 16:26:20 +0200

    Added Likes feature

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -4,12 +4,14 @@ import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs/internal/operators/tap';
 import { Register } from '../../features/account/register/register';
 import { environment } from '../../environments/environment';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likesService = inject(LikesService);
   currentUser = signal<User | null>(null);
 
   private baseUrl = environment.apiUrl;
@@ -37,11 +39,13 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user)
+    this.likesService.getLikeIds();
   }
 
   logout() {
     localStorage.removeItem('user');
     localStorage.removeItem('filters');
+    this.likesService.clearLikeIds();
     this.currentUser.set(null);
   }
 }
