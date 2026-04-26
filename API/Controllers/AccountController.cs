82d6cdd1d1866827e0ee12dab119e4fc5c316953
RadnyM <rmiko03@wp.pl>
2026-04-26 15:13:36 +0200

    Implemented unit of work pattern

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ using API.Interfaces;
 using API.Extensions;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers;
 
@@ -101,5 +102,20 @@ public class AccountController(UserManager<AppUser> userManager, ITokenService t
         };
 
         Response.Cookies.Append("refreshToken", refreshToken, cookieOptions);
+    }
+
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await userManager.Users
+            .Where(x => x.Id == User.GetMemberId())
+            .ExecuteUpdateAsync(setters => setters.SetProperty(x => x.RefreshToken, _ => null)
+            .SetProperty(x => x.RefreshTokeExpiry, _ => null)
+            );
+
+        Response.Cookies.Delete("refreshToken");
+
+        return Ok();
     }
 }
